b3c0679da910bdc65dbd82d33221e65eb6c9d3f6
MetallicDodge <108508424+MetallicDodge@users.noreply.github.com>
2023-04-29 13:33:38 +0530

    Add files via upload

---
+++ seed.ts
@@ -0,0 +1,70 @@
+import {Prisma, PrismaClient} from "@prisma/client"
+
+const client = new PrismaClient();
+
+const getaddress = ():Prisma.userCreateInput[]=>
+[
+     {
+        address_id:"30501";
+        pincode:"560021"; 
+        area_and_street:"xyz abc";
+        city_town:"Bangalore";
+        state:"Karnataka";
+        landmark:"coffee shop";
+        user:"abc";
+        order: "abc";
+      }
+
+];
+[
+    {
+       address_id:"30501";
+       pincode:"560021"; 
+       area_and_street:"xyz abc";
+       city_town:"Bangalore";
+       state:"Karnataka";
+       landmark:"coffee shop";
+       user:"abc";
+       order: "abc";
+     }
+
+];
+[
+    {
+       address_id:"30501";
+       pincode:"560021"; 
+       area_and_street:"xyz abc";
+       city_town:"Bangalore";
+       state:"Karnataka";
+       landmark:"coffee shop";
+       user:"abc";
+       order: "abc";
+     }
+
+];
+[
+    {
+       address_id:"30501";
+       pincode:"560021"; 
+       area_and_street:"xyz abc";
+       city_town:"Bangalore";
+       state:"Karnataka";
+       landmark:"coffee shop";
+       user:"abc";
+       order: "abc";
+     }
+
+];
+[
+    {
+       address_id:"30501";
+       pincode:"560021"; 
+       area_and_street:"xyz abc";
+       city_town:"Bangalore";
+       state:"Karnataka";
+       landmark:"coffee shop";
+       user:"abc";
+       order: "abc";
+     }
+
+];
